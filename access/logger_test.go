cab5be343f90c57e8063c177a6b451226719b63a
Jonathan M. Altman <jonathan@async.io>
2017-01-10 22:07:11 -0500

    Access middleware support for alternate logging
    Updated the access middleware module to enable alternate loggers
    besides passing a single line string to a logging function to
    support just one format and one type of output.  With this
    system, the existing Logger in access.Logger is still available,
    but additional loggers that support services like e.g. datadog,
    loggly, or Amazon Cloudwatch would now be possible to create.
    
    Created a new CustomLogger function that generates middleware for
    logging given a function that accepts the request and response
    writer objects and the elapsed time for processing the request.
    The function can then extract whatever data it wants about the
    processing of the request from what is available on the request
    and response writer and the elapsed time provided and output it
    anywhere it likes.
    
    CustomLogger was created by separating the middleware functionality
    and elapsed time calculation from the actual logging, which is now
    handled by the function passed in.
    
    The original Logger was then updated to use the CustomLogger by
    removing the middleware handling and elapsed time calculation parts
    of it and taking the output generation and putting that into a
    function of type LogWriterFunc.  Logger then just calls
    CustomLogger with the new output generation function as an argument
    and returns the routing.Handler CustomLogger generates.

--- access/logger_test.go
+++ access/logger_test.go
@@ -16,6 +16,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCustomLogger(t *testing.T) {
+	var buf bytes.Buffer
+	var customFunc = func(req *http.Request, rw *LogResponseWriter, elapsed float64) {
+		var logWriter = getLogger(&buf)
+		clientIP := getClientIP(req)
+		requestLine := fmt.Sprintf("%s %s %s", req.Method, req.URL.String(), req.Proto)
+		logWriter(`[%s] [%.3fms] %s %d %d`, clientIP, elapsed, requestLine, rw.Status, rw.BytesWritten)
+	}
+	h := CustomLogger(customFunc)
+
+	res := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "http://127.0.0.1/users", nil)
+	c := routing.NewContext(res, req, h, handler1)
+	assert.NotNil(t, c.Next())
+	assert.Contains(t, buf.String(), "GET http://127.0.0.1/users")
+}
+
 func TestLogger(t *testing.T) {
 	var buf bytes.Buffer
 	h := Logger(getLogger(&buf))
